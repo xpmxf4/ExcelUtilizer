c3f235d979ea85f4c3a15e57bb3668821f8a14a1
박채훈 <chaehoon.park@traport.com>
2024-07-08 16:17:32 +0900

    🎉 init: Initial Commit

---
+++ src/main/java/com/example/excelutilizer/ExcelUtilizerApplication.java
@@ -0,0 +1,13 @@
+package com.example.excelutilizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelUtilizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelUtilizerApplication.class, args);
+    }
+
+}
